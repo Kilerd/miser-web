dbafbfb00079b40b000600b59f0846b0cbee0215
Kilerd <blove694@gmail.com>
2020-10-21 17:00:39 +0800

    test: test change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -44,7 +44,7 @@
 
 <figure>
 	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
+	<figcaption>Hello Miser</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
