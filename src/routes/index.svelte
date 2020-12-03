55d743dcbb82e747da7722ad51161dd68385021c
Kilerd <blove694@gmail.com>
2020-12-03 14:45:08 +0800

    feat: update home page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <style>
     h1, figure, p {
-        text-align: center;
         margin: 0 auto;
     }
 
@@ -35,13 +34,24 @@
 </script>
 
 <svelte:head>
-    <title>Sapper project template</title>
+    <title>Project Miser</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Project Miser</h1>
 
 <figure>
     <figcaption>Hello Miser</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong>An double-entry accounting tool with friendly ui and powerful statistics graphic</strong></p>
+<p>aim to:</p>
+<ul>
+    <li>scheduler transaction supported</li>
+    <li>auto commodity's price update</li>
+    <li>transactions' documents supported</li>
+    <li>developer friendly, fully api supported for automation tool</li>
+    <li>beancount exportation supported</li>
+    <li>encrypted payee and narration supported</li>
+</ul>
+
+<p>this project is now totally <strong>WIP</strong>, and the register page is hidden. if you wanna try the project, you can register it and then taste, if you can find how to register(PS: I promise that the api of registering is working well). any bug, recommendation, feedback or features are welcome via blove694@gmail.com.</p>
