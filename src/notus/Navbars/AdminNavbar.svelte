ed836153223f184951dc5eca9fd41ec372474fed
Kilerd <blove694@gmail.com>
2020-12-09 18:30:12 +0800

    feat: larger title font size

--- src/notus/Navbars/AdminNavbar.svelte
+++ src/notus/Navbars/AdminNavbar.svelte
@@ -6,34 +6,19 @@
 </script>
 
 <!-- Navbar -->
-<nav
-  class="w-full z-10 bg-red-500 md:flex-row md:flex-no-wrap md:justify-start flex items-center p-4"
->
-  <div
-    class="w-full mx-autp items-center flex justify-between md:flex-no-wrap flex-wrap md:px-10 px-4"
-  >
+<nav class="w-full z-10 bg-red-500 md:flex-row md:flex-no-wrap md:justify-start flex items-center p-4">
+  <div class="w-full mx-autp items-center flex justify-between md:flex-no-wrap flex-wrap md:px-10 px-4">
     <!-- Brand -->
-    <a
-      class="text-white text-sm uppercase hidden lg:inline-block font-semibold"
-      href="#pablo" on:click={(e) => e.preventDefault()}
-    >
+    <h2 class="text-white text-3xl uppercase hidden lg:inline-block font-semibold">
       {title}
-    </a>
+    </h2>
     <!-- Form -->
-    <form
-      class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"
-    >
+    <form class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
       <div class="relative flex w-full flex-wrap items-stretch">
-        <span
-          class="z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3"
-        >
+        <span class="z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
           <i class="fas fa-search"></i>
         </span>
-        <input
-          type="text"
-          placeholder="Search here..."
-          class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pl-10"
-        />
+        <input type="text" placeholder="Search here..." class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pl-10"/>
       </div>
     </form>
     <!-- User -->
