634e0c1fb9d99e42967e552147d883300627be81
Kilerd <blove694@gmail.com>
2020-12-30 00:15:51 +0800

    fix: create transaction modal fix

--- src/components/NewTransactionModal.svelte
+++ src/components/NewTransactionModal.svelte
@@ -6,10 +6,17 @@
     import type {Account} from '../types';
 
     export let modalClose;
+
     interface AccountSelectItem {
         id: number,
         keyword: string,
         alias: string
+    }
+
+    interface Line {
+        account?: AccountSelectItem,
+        amount: string,
+        currency: string,
     }
 
     let accountList: Account[] = [];
@@ -25,41 +32,35 @@
         ]
     }
 
-    let isSubmit = false;
     let defaultDate = new Date();
     defaultDate.setMinutes(defaultDate.getMinutes() - defaultDate.getTimezoneOffset());
-    let date = defaultDate.toJSON().slice(0, 10)
-    let payee = '';
-    let narration = '';
 
-    let selectAccount: AccountSelectItem;
-    let amount: string = '';
-    let selectAccount2: AccountSelectItem;
-    let amount2: string = '';
+    let base = {date: defaultDate.toJSON().slice(0, 10), payee: '', narration: ''};
+    let lines: Line[] = [{
+        account: undefined,
+        amount: '',
+        currency: 'CNY'
+    }, {
+        account: undefined,
+        amount: '',
+        currency: 'CNY'
+    }]
+    let isSubmit = false;
 
-    $: canBeSubmit = selectAccount !== undefined && amount !== '' && selectAccount2 !== undefined && amount2 !== '';
+    $: lineHasEmpty = lines.filter(it => it.account === undefined || it.amount === '' || it.amount === null).length > 0;
+    $: canBeSubmit = !lineHasEmpty;
     $: submitDisable = !canBeSubmit || isSubmit;
 
     async function submit() {
+        console.log(base, lines);
         isSubmit = true;
-        let lines = [
-            {
-                account: selectAccount.id,
-                amount: [
-                    amount,
-                    'CNY'
-                ]
-            },
-            {
-                account: selectAccount2.id,
-                amount: [
-                    amount2,
-                    'CNY'
-                ]
-            }
-        ]
 
-        await api.createTransaction(date, payee, narration, [], [], lines)
+        let lineRes = lines.map(it => ({
+            account: it.account.id,
+            amount: [it.amount, it.currency]
+        }));
+
+        await api.createTransaction(base.date, base.payee, base.narration, [], [], lineRes)
         isSubmit = false;
         modalClose();
     }
@@ -71,10 +72,6 @@
 </script>
 
 <style>
-    div.line {
-        display: flex;
-        flex-direction: row;
-    }
 
     div :global(.autocomplete) {
         flex: 1 auto;
@@ -98,14 +95,14 @@
                         <input class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
                                type="date"
                                name="date"
-                               bind:value={date}
+                               bind:value={base.date}
                                placeholder="2020-10-10"/>
                     </div>
                 </div>
                 <div class="w-full lg:w-4/12 px-4">
                     <div class="relative w-full mb-3">
 
-                        <input bind:value={payee} placeholder="Payee"
+                        <input bind:value={base.payee} placeholder="Payee"
                                type="text"
                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
                         />
@@ -113,7 +110,7 @@
                 </div>
                 <div class="w-full lg:w-4/12 px-4">
                     <div class="relative w-full mb-3">
-                        <input bind:value={narration} placeholder="Narration"
+                        <input bind:value={base.narration} placeholder="Narration"
                                type="text"
                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
                         />
@@ -127,59 +124,36 @@
             <h6 class="text-gray-500 text-sm mt-3 mb-6 font-bold uppercase">
                 Detail
             </h6>
-            <div class="flex flex-wrap">
-                <div class="w-full lg:w-4/12 px-4">
-                    <div class="relative w-full mb-3">
-                        <AutoComplete inputId="account1" className="account-input" items={accountList}
-                                      labelFunction={itemShow}
-                                      valueFieldName="name"
-                                      bind:selectedItem={selectAccount}
-                                      searchFunction={getItems} showClear={true}/>
+            {#each lines as line}
+                <div class="flex flex-wrap">
+                    <div class="w-full lg:w-4/12 px-4">
+                        <div class="relative w-full mb-3">
+                            <AutoComplete inputId="account1" className="account-input" items={accountList}
+                                          labelFunction={itemShow}
+                                          valueFieldName="name"
+                                          bind:selectedItem={line.account}
+                                          searchFunction={getItems} showClear={true}/>
+                        </div>
                     </div>
-                </div>
-                <div class="w-full lg:w-4/12 px-4">
-                    <div class="relative w-full mb-3">
-                        <input bind:value={amount} type="number"
-                               class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"/>
+                    <div class="w-full lg:w-4/12 px-4">
+                        <div class="relative w-full mb-3">
+                            <input bind:value={line.amount} type="number"
+                                   class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"/>
 
 
+                        </div>
+                    </div>
+                    <div class="w-full lg:w-4/12 px-4">
+                        <div class="relative w-full mb-3">
+                            <select name="select" id="exampleSelect"
+                                    class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150">
+                                <option>CNY</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
-                <div class="w-full lg:w-4/12 px-4">
-                    <div class="relative w-full mb-3">
-                        <select name="select" id="exampleSelect"
-                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150">
-                            <option>CNY</option>
-                        </select>
-                    </div>
-                </div>
+            {/each}
 
-                <div class="w-full lg:w-4/12 px-4">
-                    <div class="relative w-full mb-3">
-                        <AutoComplete inputId="account1" className="account-input" items={accountList}
-                                      labelFunction={itemShow}
-                                      valueFieldName="name"
-                                      bind:selectedItem={selectAccount2}
-                                      searchFunction={getItems} showClear={true}/>
-                    </div>
-                </div>
-                <div class="w-full lg:w-4/12 px-4">
-                    <div class="relative w-full mb-3">
-                        <input bind:value={amount2} type="number"
-                               class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"/>
-
-
-                    </div>
-                </div>
-                <div class="w-full lg:w-4/12 px-4">
-                    <div class="relative w-full mb-3">
-                        <select name="select"
-                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150">
-                            <option>CNY</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
 
             <hr class="mt-6 mb-2 border-b-1 border-gray-400"/>
 
@@ -187,11 +161,11 @@
                 <button
                         disabled={submitDisable}
                         on:click={submit}
-                        class="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                        class="{canBeSubmit ? "bg-red-500": "bg-gray-700"} text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                         type="button"
                 >
                     {#if isSubmit}
-                        <Spinner color="light" size="sm"/>
+                        Creating....
                     {:else}
                         Create
                     {/if}
