d60889e0f8ecaf9306c538a28bb65b872bfa4a89
Kilerd <blove694@gmail.com>
2021-01-02 12:14:44 +0800

    feat: update static bar

--- src/notus/Cards/CardBarChart.svelte
+++ src/notus/Cards/CardBarChart.svelte
@@ -9,6 +9,124 @@
 
     // init chart
     onMount(async () => {
+        let data = {};
+        for (let i = 0; i < 30; i++) {
+            let today = Dayjs();
+            let day = today.subtract(i, 'day');
+            data[day.format('YYYY-MM-DD')] = {day: day, label: day.format('MM-DD'), amount: new Big(0)};
+        }
+
+        for (let [day, dayTransactions] of $directives.entries) {
+            let targetDay = data[day];
+            console.log(day, dayTransactions, targetDay);
+            if (targetDay !== undefined) {
+                for (let it of dayTransactions) {
+                    for (let line of it.lines) {
+                        const accountId = line.account;
+                        const type = $accounts[accountId]?.full_name.split(':')[0];
+                        const cost = new Big(line.cost[0]);
+                        switch (type) {
+                            case 'Income':
+                                targetDay.amount = targetDay.amount.sub(cost);
+                                break;
+                            case 'Expenses':
+                                targetDay.amount = targetDay.amount.sub(cost);
+                                break;
+                            case 'Liabilities':
+                                targetDay.amount = targetDay.amount.sub(cost);
+                                break;
+                            case 'Equity':
+                                break;
+                            case 'Assets':
+                                // targetDay.amount = targetDay.amount.sub(cost);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        let labels = Object.values(data).sort((a, b) => a.day - b.day).map(it => it.label);
+        let dataset = Object.values(data).sort((a, b) => a.day - b.day).map(it => it.amount);
+        let config = {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: new Date().getFullYear(),
+                        backgroundColor: 'rgba(237,100,166, 0.5)',
+                        borderColor: 'rgba(237,100,166, 1)',
+                        data: dataset,
+                        fill: false,
+                        borderWidth: 1,
+                    }
+                ]
+            },
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Orders Chart',
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true,
+                },
+                legend: {
+                    labels: {
+                        fontColor: 'rgba(0,0,0,.4)',
+                    },
+                    align: 'end',
+                    position: 'bottom',
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            display: true,
+                            scaleLabel: {
+                                display: false,
+                                labelString: 'Month',
+                            },
+                            gridLines: {
+                                borderDash: [2],
+                                borderDashOffset: [0],
+                                color: 'rgba(33, 37, 41, 0.3)',
+                                zeroLineColor: 'rgba(33, 37, 41, 0.3)',
+                                zeroLineBorderDash: [2],
+                                zeroLineBorderDashOffset: [0],
+                            },
+                        },
+                    ],
+                    yAxes: [
+                        {
+                            display: true,
+                            scaleLabel: {
+                                display: false,
+                                labelString: 'Value',
+                            },
+                            gridLines: {
+                                borderDash: [2],
+                                drawBorder: false,
+                                borderDashOffset: [2],
+                                color: 'rgba(33, 37, 41, 0.2)',
+                                zeroLineColor: 'rgba(33, 37, 41, 0.15)',
+                                zeroLineBorderDash: [3],
+                                zeroLineBorderDashOffset: [2],
+                            },
+                        },
+                    ],
+                },
+            },
+        };
+        let ctx = document.getElementById('bar-chart').getContext('2d');
+        let chart = new Chart(ctx, config);
+        window.myBar = chart;
 
         directives.subscribe(newStoreValue=> {
             let data = {};
@@ -18,8 +136,7 @@
                 data[day.format('YYYY-MM-DD')] = {day: day, label: day.format('MM-DD'), amount: new Big(0)};
             }
 
-            console.log("value", newStoreValue.entries);
-            for (let [day, dayTransactions] of newStoreValue.entries) {
+            for (let [day, dayTransactions] of $directives.entries) {
                 let targetDay = data[day];
                 console.log(day, dayTransactions, targetDay);
                 if (targetDay !== undefined) {
@@ -50,85 +167,12 @@
             }
 
             let labels = Object.values(data).sort((a, b) => a.day - b.day).map(it => it.label);
-            let dataset = Object.values(data).sort((a, b) => a.day - b.day).map(it => it.amount);
-            let config = {
-                type: 'bar',
-                data: {
-                    labels: labels,
-                    datasets: [
-                        {
-                            label: new Date().getFullYear(),
-                            backgroundColor: 'rgba(237,100,166, 0.5)',
-                            borderColor: 'rgba(237,100,166, 1)',
-                            data: dataset,
-                            fill: false,
-                            borderWidth: 1,
-                        }
-                    ]
-                },
-                options: {
-                    maintainAspectRatio: false,
-                    responsive: true,
-                    title: {
-                        display: true,
-                        text: 'Orders Chart',
-                    },
-                    tooltips: {
-                        mode: 'index',
-                        intersect: false,
-                    },
-                    hover: {
-                        mode: 'nearest',
-                        intersect: true,
-                    },
-                    legend: {
-                        labels: {
-                            fontColor: 'rgba(0,0,0,.4)',
-                        },
-                        align: 'end',
-                        position: 'bottom',
-                    },
-                    scales: {
-                        xAxes: [
-                            {
-                                display: true,
-                                scaleLabel: {
-                                    display: false,
-                                    labelString: 'Month',
-                                },
-                                gridLines: {
-                                    borderDash: [2],
-                                    borderDashOffset: [0],
-                                    color: 'rgba(33, 37, 41, 0.3)',
-                                    zeroLineColor: 'rgba(33, 37, 41, 0.3)',
-                                    zeroLineBorderDash: [2],
-                                    zeroLineBorderDashOffset: [0],
-                                },
-                            },
-                        ],
-                        yAxes: [
-                            {
-                                display: true,
-                                scaleLabel: {
-                                    display: false,
-                                    labelString: 'Value',
-                                },
-                                gridLines: {
-                                    borderDash: [2],
-                                    drawBorder: false,
-                                    borderDashOffset: [2],
-                                    color: 'rgba(33, 37, 41, 0.2)',
-                                    zeroLineColor: 'rgba(33, 37, 41, 0.15)',
-                                    zeroLineBorderDash: [3],
-                                    zeroLineBorderDashOffset: [2],
-                                },
-                            },
-                        ],
-                    },
-                },
-            };
-            let ctx = document.getElementById('bar-chart').getContext('2d');
-            window.myBar = new Chart(ctx, config);
+            let updatedDataset = Object.values(data).sort((a, b) => a.day - b.day).map(it => it.amount);
+            chart.data.labels = labels;
+            chart.data.datasets.forEach((dataset)=> {
+                dataset.data = updatedDataset;
+            })
+            chart.update();
         })
 
 
